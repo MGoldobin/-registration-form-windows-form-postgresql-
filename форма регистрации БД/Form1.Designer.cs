dd8ff4573987b0dd6ca9a0ba56147c33202b0956
MGoldobin <47533332+MGoldobin@users.noreply.github.com>
2020-03-29 19:13:05 +0500

    Add files via upload

---
+++ форма регистрации БД/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace форма_регистрации_БД
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonReg = new System.Windows.Forms.Button();
+            this.labelError = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(87, 64);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(130, 20);
+            this.textBoxLogin.TabIndex = 0;
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.Location = new System.Drawing.Point(87, 90);
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.Size = new System.Drawing.Size(130, 20);
+            this.textBoxPass.TabIndex = 1;
+            this.textBoxPass.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Login:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Password:";
+            // 
+            // buttonReg
+            // 
+            this.buttonReg.Location = new System.Drawing.Point(87, 116);
+            this.buttonReg.Name = "buttonReg";
+            this.buttonReg.Size = new System.Drawing.Size(62, 23);
+            this.buttonReg.TabIndex = 4;
+            this.buttonReg.Text = "Sign up";
+            this.buttonReg.UseVisualStyleBackColor = true;
+            this.buttonReg.Click += new System.EventHandler(this.buttonReg_Click_1);
+            // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Location = new System.Drawing.Point(36, 173);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(0, 13);
+            this.labelError.TabIndex = 5;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(155, 116);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(62, 23);
+            this.buttonLogin.TabIndex = 6;
+            this.buttonLogin.Text = "Log in";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click_1);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(273, 272);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.labelError);
+            this.Controls.Add(this.buttonReg);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPass);
+            this.Controls.Add(this.textBoxLogin);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        //private System.Windows.Forms.TextBox textBox1;
+        //private System.Windows.Forms.Label label1;
+        //private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonReg;
+        private System.Windows.Forms.Label labelError;
+        private System.Windows.Forms.Button buttonLogin;
+    }
+}
+
---
+++ форма регистрации БД/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using System.Security.Cryptography;
+
+namespace форма_регистрации_БД
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string hash(string password)
+        {
+            //переводим строку в байт-массим  
+            byte[] bytes = Encoding.Unicode.GetBytes(password);
+
+            //создаем объект для получения средст шифрования  
+            MD5CryptoServiceProvider CSP =
+                new MD5CryptoServiceProvider();
+
+            //вычисляем хеш-представление в байтах  
+            byte[] byteHash = CSP.ComputeHash(bytes);
+
+            string hash = string.Empty;
+
+            //формируем одну цельную строку из массива  
+            foreach (byte b in byteHash)
+                hash += string.Format("{0:x2}", b);
+
+            return hash;
+        }
+        string salt = "ILDB";
+
+        private async void buttonReg_Click_1(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxLogin.Text) && !string.IsNullOrWhiteSpace(textBoxLogin.Text) &&
+                !string.IsNullOrEmpty(textBoxPass.Text) && !string.IsNullOrWhiteSpace(textBoxPass.Text))
+            {
+                string login = textBoxLogin.Text;
+                string password = hash(textBoxPass.Text + salt);
+                string ConnectionString = @"Host = localhost;Username = postgres;Password = Misha310; Database = postgres";
+                var Connect = new NpgsqlConnection(ConnectionString);
+                Connect.Open();
+                NpgsqlCommand Command = new NpgsqlCommand("insert into users(login, password) values (@login, @password)", Connect);
+                Command.Parameters.AddWithValue("login", login);
+                Command.Parameters.AddWithValue("password", password);
+                try
+                {
+                    await Command.ExecuteNonQueryAsync();
+                    labelError.Text = "Пользоватьтель с логином " + login + " зарегистрирован";
+                }
+                catch
+                {
+                    labelError.Text = "Пользоватьтель с логином " + login + "  уже существует";
+                }
+                finally
+                {
+                    textBoxLogin.Text = "";
+                    textBoxPass.Text = "";
+                    Connect.Close();
+                }
+            }
+            else labelError.Text = "Поля 'Login'  и  'Password' должны быть заполнены!";
+
+        }
+
+        private void buttonLogin_Click_1(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxLogin.Text) && !string.IsNullOrWhiteSpace(textBoxLogin.Text) &&
+                  !string.IsNullOrEmpty(textBoxPass.Text) && !string.IsNullOrWhiteSpace(textBoxPass.Text))
+            {
+                string login = textBoxLogin.Text;
+                string password = hash(textBoxPass.Text + salt);
+                string ConnectionString = @"Host = localhost;Username = postgres;Password = Misha310; Database = postgres";
+                NpgsqlConnection Connect = new NpgsqlConnection(ConnectionString);
+                Connect.Open();
+                var Command = new NpgsqlCommand("select password from users where login = @login", Connect);
+                Command.Parameters.AddWithValue("login", login);
+                var Reader = Command.ExecuteReader();
+                if (Reader.Read() == true)
+                {
+                    if (Reader.GetString(0) == password)
+                    {
+                        labelError.Text = "Logged in";
+                        textBoxLogin.Text = "";
+                        textBoxPass.Text = "";
+                    }
+                    else
+                    {
+                        labelError.Text = "Incorrect password";
+                        textBoxPass.Text = "";
+                    }
+                }
+                textBoxLogin.Text = "";
+                textBoxPass.Text = "";
+                Connect.Close();
+            } else labelError.Text = "Поля 'Login'  и  'Password' должны быть заполнены!";
+        }
+        
+    }
+}
